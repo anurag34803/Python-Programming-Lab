16abddd6ebc6905b0359cf4dccd545dddb76a1fe
anurag34803 <48412537+anurag34803@users.noreply.github.com>
2022-05-25 19:14:55 +0530

    Add files via upload

---
+++ Greatest Between Three Number.py
@@ -0,0 +1,17 @@
+a = int (input ("Enter First Number\n"))
+b = int(input("Enter Second Number\n"))
+c = int(input("Enter Third Number\n"))
+
+if(a>b):
+    if(a>c):
+        print("First Is Greater")
+        
+    else:
+        print("Third is Greater")
+        
+else:
+    if(b>c):
+        print("Second Is Greater")
+        
+    else:
+        print("Third is Greater")
