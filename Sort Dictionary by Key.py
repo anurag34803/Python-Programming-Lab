e1c252069253aef19a47238afefcf528e03a2df0
anurag34803 <48412537+anurag34803@users.noreply.github.com>
2022-05-26 23:46:24 +0530

    Add files via upload

---
+++ Sort Dictionary by Key.py
@@ -0,0 +1,10 @@
+dct = {
+       'Apple' : 1,
+       'Mango' : 4 ,
+       'Grapes' : 120,
+       'Banana' : 6 ,
+       'Cheery' : 24
+    }
+    
+for i in sorted (dct.keys()) :
+     print(i, end = " ")
