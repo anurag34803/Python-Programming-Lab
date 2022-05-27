a9cdcdb7059e639956f2236a0cb0eb9bd48dfc9e
anurag34803 <48412537+anurag34803@users.noreply.github.com>
2022-05-27 23:37:00 +0530

    Add files via upload

---
+++ Convert tuple into string.py
@@ -0,0 +1,9 @@
+tup = (5,4,3,2,1,11,10,9,8)                      #Creation of tuple
+print(f"Tuple is {tup}")
+
+string = str()
+for i in tup:
+    i = str(i)
+    string = string+i
+    
+print(string)
